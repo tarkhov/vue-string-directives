dcff59c58de4d9c76eab557d16ac8b1443a646cb
tarkhov <agtuks@gmail.com>
2020-08-31 12:46:37 +0300

    Close #11

--- src/directives/upper.js
+++ src/directives/upper.js
@@ -5,10 +5,11 @@ export default {
   componentUpdated: function (el, binding) {
     if (el.value.length && typeof binding.value === 'undefined' || binding.value) {
       if (binding.modifiers?.first) {
+        const capitalize = binding.modifiers?.capitalize
         if (binding.modifiers?.every) {
-          el.value = upperEvery(el.value)
+          el.value = upperEvery(el.value, capitalize)
         } else {
-          el.value = upperFirst(el.value)
+          el.value = upperFirst(el.value, capitalize)
         }
       } else {
         el.value = el.value.toUpperCase()
