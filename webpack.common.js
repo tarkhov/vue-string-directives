9304a8ec6ccaf498c8d3941514132587442aaa6b
tarkhov <agtuks@gmail.com>
2020-09-02 10:01:39 +0300

    commit

--- webpack.common.js
+++ webpack.common.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'vue-string-directives.min.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'vue-string-directives.common.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
